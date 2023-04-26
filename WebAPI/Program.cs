85419fc2a751fbd54d19a2cf441b5636b7255729
Zynad <zynadlol@gmail.com>
2023-04-26 10:40:51 +0200

    DB + connstring added

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DataDB")));
 
 var app = builder.Build();
 
