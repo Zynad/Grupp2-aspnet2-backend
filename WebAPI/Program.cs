f075be038c7f0a9a25dbfb3aa7551654d4fa0c85
Zynad <zynadlol@gmail.com>
2023-04-28 09:58:06 +0200

    Identity and jwt token

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WebAPI.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +13,46 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DataDB")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(x =>
+{
+    x.Password.RequiredLength = 8;
+    x.SignIn.RequireConfirmedAccount = false;
+    x.User.RequireUniqueEmail = true;
+
+}).AddEntityFrameworkStores<DataContext>();
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.Events = new JwtBearerEvents
+    {
+        OnTokenValidated = context =>
+        {
+            if (string.IsNullOrEmpty(context?.Principal?.FindFirst("id")?.Value) || string.IsNullOrEmpty(context?.Principal?.Identity?.Name))
+                context?.Fail("Unauthorized");
+
+            return Task.CompletedTask;
+        }
+    };
+
+    x.RequireHttpsMetadata = true;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration.GetSection("TokenValidation").GetValue<string>("Issuer")!,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration.GetSection("TokenValidation").GetValue<string>("Audience")!,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration.GetSection("TokenValidation").GetValue<string>("SecretKey")!))
+    };
+});
 
 var app = builder.Build();
 
