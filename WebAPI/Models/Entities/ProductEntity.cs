59ca40380b4a0f957a10781a91c9f4899f39573b
Tobias Westman <ctvwestman@gmail.com>
2023-05-04 19:46:28 +0200

    product updates 2

--- WebAPI/Models/Entities/ProductEntity.cs
+++ WebAPI/Models/Entities/ProductEntity.cs
@@ -5,30 +5,19 @@ namespace WebAPI.Models.Entities
 {
 	public class ProductEntity
 	{
-		[Key]
 		public Guid Id { get; set; } = Guid.NewGuid();
-
 		public string Category { get; set; }
-
 		public List<string> Tags { get; set; }
-
 		[MaxLength(100)]
 		public string Name { get; set; } = null!;
-
 		[Range(0, double.MaxValue)]
 		[DataType(DataType.Currency)]
 		public double Price { get; set; }
-
 		public string ImageUrl { get; set; } = null!;
-
 		public string Description { get; set; } = null!;
-
 		public int Rating { get; set; }
-
 		public string Brand { get; set; } = null!;
-
 		public bool Discount { get; set; }
-
 		public double? DiscountMultiplier { get; set; }
 		
 		public string PartitionKey => Id.ToString();
