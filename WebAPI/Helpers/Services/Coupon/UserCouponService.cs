3755818d952834737b3246c50147cd494d80e80d
Emily <Ghazaleh.beheshtian@gmail.com>
2023-05-21 22:15:13 +0200

    UserCoupon

--- WebAPI/Helpers/Services/Coupon/UserCouponService.cs
+++ WebAPI/Helpers/Services/Coupon/UserCouponService.cs
@@ -1,6 +1,22 @@
-﻿namespace WebAPI.Helpers.Services.Coupon
+﻿using WebAPI.Helpers.Repositories;
+using WebAPI.Models.Entities;
+using System.Collections;
+using WebAPI.Models.Dtos;
+using WebAPI.Models.Entities;
+using WebAPI.Models.Schemas;
+using WebAPI.Helpers.Repositories.BaseModels;
+
+namespace WebAPI.Helpers.Services.Coupon
 {
     public class UserCouponService
     {
+        private readonly UserCouponRepo _userCouponRepo;
+
+        public UserCouponService(UserCouponRepo userCouponRepo)
+        {
+            _userCouponRepo = userCouponRepo;
+        }
+
+  
     }
 }
