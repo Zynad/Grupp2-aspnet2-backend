88e7a686f9c9cfa9f022de183aa23d1781cb1423
Zynad <zynadlol@gmail.com>
2023-05-25 22:46:31 +0200

    Ready for unit tests

---
+++ WebAPI.Tests/Units/Controllers/UserCouponControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+using WebAPI.Models.Schemas;
+using WebAPI.Models.Interfaces;
+using Xunit;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+using WebAPI.Models.Entities;
+using WebAPI.Models.Dtos;
+
+namespace WebAPI.Tests.Controllers;
+
+//Alex testar denna
+public class UserCouponControllerTests
+{
+    private Mock<IUserCouponService> _mockUserCouponService;
+    private UserCouponController _controller;
+
+    public UserCouponControllerTests()
+    {
+        _mockUserCouponService = new Mock<IUserCouponService>();
+        _controller = new UserCouponController(_mockUserCouponService.Object);
+    }
+}
