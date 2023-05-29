9170ed680ac2a6c0bf50b8d88e2f6547c2ae83ce
Tobias Westman <ctvwestman@gmail.com>
2023-05-29 11:23:14 +0200

    Added confirmaiton email to createorder

--- WebAPI/Models/Entities/OrderItemEntity.cs
+++ WebAPI/Models/Entities/OrderItemEntity.cs
@@ -6,7 +6,7 @@ public class OrderItemEntity
     public Guid ProductId { get; set; }
     public string ProductName { get; set; } = null!;
     public double UnitPrice { get; set; }
-    public string ImageUrl { get; set; }
+    public string? ImageUrl { get; set; }
     public int Quantity { get; set; }
     public string? Color { get; set; }
     public string? Size { get; set; }
